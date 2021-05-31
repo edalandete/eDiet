be3523b5ae672cdb752e51a1e8e716afffd49d04
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-05-31 16:49:19 +0200

    Setup auth with passport create local strategy passport create local strategy jwt create dietician model create auth routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 require('dotenv').config();
+const authRoutes = require('./routes/auth.routes')();
 
 require('./ddbb/mongoose.config');
 
 const app = express();
 const port = process.env.PORT || 4000;
+
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require('./passport/passport.config')(app);
+
+app.use('/', authRoutes);
+
 app.listen(port, debug(`server is running on port ${port}`));
---
+++ server/src/passport/strategies/local.strategy.js
@@ -0,0 +1,77 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const md5 = require('md5');
+const Dietician = require('../../models/dietician.model');
+
+module.exports = function localStrategyConfig() {
+  passport.use(
+    'signup',
+    new LocalStrategy.Strategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+
+      },
+
+      async (email, password, done) => {
+        try {
+          const existingUser = await Dietician.findOne({ email });
+          if (existingUser) {
+            return done(null, false, { message: 'User alredy exists' });
+          }
+
+          const user = await Dietician.create({ email, password: md5(password) });
+
+          return done(null, user);
+        } catch (error) {
+          return done(error);
+        }
+      },
+    ),
+  );
+  passport.use(
+    'login',
+    new LocalStrategy.Strategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+      },
+      async (email, password, done) => {
+        try {
+          const user = await Dietician.findOne({ email });
+
+          if (!user) {
+            return done(null, false, { message: 'User not found' });
+          }
+
+          const validate = await user.verifyPassword(password);
+
+          if (!validate) {
+            return done(null, false, { message: 'Wrong Password' });
+          }
+
+          return done(null, user, { message: 'Logged in Successfully' });
+        } catch (error) {
+          return done(error);
+        }
+      },
+    ),
+  );
+  passport.use(
+    new JWTstrategy(
+      {
+        secretOrKey: process.env.SECRET_OR_KEY,
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      },
+      async (token, done) => {
+        try {
+          return done(null, token.user);
+        } catch (error) {
+          return done(error);
+        }
+      },
+    ),
+  );
+};
