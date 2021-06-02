886863e179fe2215d1d13730702f028447c48935
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-06-02 16:49:25 +0200

    Create frontend application

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  signupUrl: 'http://localhost:2516/signup',
+  loginUrl: 'http://localhost:2516/login',
+  dieticiansUrl: 'http://localhost:2516/api/dieticians/',
+  dietsUrl: 'http://localhost:2516/api/diets/',
+  appointmentsUrl: 'http://localhost:2516/api/appointments/',
+  patientsUrl: 'http://localhost:2516/api/patients/'
+};
