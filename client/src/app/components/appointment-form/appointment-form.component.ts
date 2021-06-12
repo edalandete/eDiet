220fa8ee131274b04d65e0068a8218695b4ba001
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-06-12 11:48:11 +0200

    Create appoinrtment form structure

--- client/src/app/components/appointment-form/appointment-form.component.ts
+++ client/src/app/components/appointment-form/appointment-form.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+import { StoreService } from 'src/app/core/services/store/store.service';
+import { Appointment } from 'src/app/core/models/appointment.model';
 
 @Component({
   selector: 'app-appointment-form',
   templateUrl: './appointment-form.component.html',
-  styleUrls: ['./appointment-form.component.scss']
+  styleUrls: ['./appointment-form.component.scss', './../../app.component.scss']
 })
 export class AppointmentFormComponent implements OnInit {
 
-  constructor() { }
+  newAppointmentForm: FormGroup = this.formBuilder.group({
+
+  })
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public storeService: StoreService,
+    ) { }
 
   ngOnInit(): void {
+    this.detectFormChanges();
+  }
+
+  createAppointment() {
+
+  }
+
+  detectFormChanges(): void {
+    this.newAppointmentForm.valueChanges
+    .pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      tap((formValue)=> this.storeService.patient$.next(formValue))
+    )
+    .subscribe();
   }
 
 }
