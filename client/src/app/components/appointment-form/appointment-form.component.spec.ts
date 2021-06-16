52ed4f76f0e80c4bbe5eebd1902389b203aca29c
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-06-16 10:22:22 +0200

    Test appointment form

--- client/src/app/components/appointment-form/appointment-form.component.spec.ts
+++ client/src/app/components/appointment-form/appointment-form.component.spec.ts
@@ -7,6 +7,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppointmentFormComponent } from './appointment-form.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Appointment } from 'src/app/core/models/appointment.model';
+import { PatientDetailComponent } from '../patient-detail/patient-detail.component';
 
 describe('AppointmentFormComponent', () => {
   let component: AppointmentFormComponent;
@@ -57,19 +59,29 @@ describe('AppointmentFormComponent', () => {
     },
     isActive: true,
   };
+  const hours: string [] = ["10:00"];
+
+  const appointment: Appointment = {
+    _id: "fgff",
+    dieticianId: "fkdvmlkf",
+    patient: patient,
+    date: "22/05/2021",
+    time: "10:00"
+  }
 
   const storeServiceMock = {
     createPatient: () => of(),
-    selectedPatient$: new BehaviorSubject<Patient>(patient)
+    selectedPatient$: new BehaviorSubject<Patient>(patient),
+    getAvailableHours: () => of(hours)
   };
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AppointmentFormComponent ],
-      imports: [HttpClientTestingModule, ReactiveFormsModule, RouterTestingModule],
-      providers: [
-        {provide: StoreService, useValue: storeServiceMock},
-      ]
+      imports: [HttpClientTestingModule, ReactiveFormsModule, RouterTestingModule.withRoutes(
+        [{path: 'detail/123', component: PatientDetailComponent}])],
+      providers: [StoreService]
     })
     .compileComponents();
   });
@@ -79,6 +91,32 @@ describe('AppointmentFormComponent', () => {
     component = fixture.componentInstance;
     storeService = TestBed.inject(StoreService);
     fixture.detectChanges();
+  });
+
+  it("And the go back button is pressed", () => {
+    const spyFn = spyOn(component.componentsHelper,'goToDetail');
+    component.cancel();
+    expect(spyFn).toHaveBeenCalled();
+  });
+
+  it("And the selected date is changed", () => {
+    const spyFn = spyOn(component.storeService,'getAvailableHours').and.returnValue(of(hours));
+    component.onDateChange();
+    expect(spyFn).toHaveBeenCalled();
+  });
+
+  it("And the appointment is created", () => {
+    component.patient._id = '123';
+    const spyFn = spyOn(component.storeService,'createAppointment').and.returnValue(of(appointment));;
+    component.createAppointment();
+    expect(spyFn).toHaveBeenCalled();
+  });
+
+  it("And the time is changed", () => {
+    component.patient._id = '123';
+    const spyFn = spyOn(component.availableHours,'find').and.returnValue("10:00");;
+    component.changeTime("event");
+    expect(component.selectedHour).toBe("10:00");
   });
 
   it('should create', () => {
